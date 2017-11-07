140947727646e06c28321f0de349dab31e926f1c
Ahmet Hamza Emra <hamza.emra@gmail.com>
2017-11-07 12:19:01 -0600

    all

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# MLRPRO
-Resume Check with Machine Learning 
+# MLRP Project for HackHouston 2017
+
+
+
+# Online resume processing application using machine learning algorithms
+------------------------------------------------------------------------
+## Inspiration
+As a team of undergrad computer scientists we will soon face the crazy world or resumes, interviews, work, etc... It stresses everyone out whether they will be able to get that dream job at Google or just to for the sake of competition.
+
+## What it does
+Our machine learning system uses unsupervised learning algorithms to cluster resumes of top software engineers in various companies and label their skills accordingly. Next, the user uploads their resume and it is tested against the predicted hypothesis by the ML algorithms which in hand return what tier(s) you belong to. As for tiers, we divided them into two parts. Top tier, such as Google, Facebook, Amazon, IBM, etc..., and bottom tier such as local Houston companies. Unfortunately, due to time constraints we cannot provide company specific statistics according to ones resume. (We plan to offer location based ranking and statistics in the future). Another factor we included was education and improvement. After viewing the results, users can follow recommended links provided to websites where they can practice on algorithms or take online certifications that can help them stand out even further!
+
+## How we built it
+We used scikit-learn module K Means Clustering Next we se tup the backend using Flask and published it on PythonAnyWhere. The frontend is built using HTML5/CSS3. 
+
+## Challenges we ran into
+Finding resumes! In order for our system to work accurately we must provide as much training data (resumes) as possible! And not only that but quality resumes! Such things are hard to come across. Luckily, we managed to come up with 1000 resumes. After some cleaning and preprocessing we shrink the number to 300. Right now, because resumes are personal data, We cannot put them as they are. Ergo, the data is preprocesed with stemming and cleaned from the stop words and stored in csv file.
+
+## Accomplishments that we're proud of
+We are really proud with the idea that we came up with because many students are worried about their future and would like to know what the future holds for them. Unfortunately, we are no fortune tellers but nevertheless we are skillful machine learning engineers who with some help of statistics, machine learning tools and Python can help anyone discover their real potential!
+
+## What's next for MLRP
+We would like to dive deeper into machine learning algorithms to provide more descriptive and personalized results to every single user for a full user friendly experience.
+
+## How to run 
+
+```
+python3 flask_app.py
+```
+
+
+
---
+++ flask_app.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import re
+import os
+import PyPDF2
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from flask import render_template, Flask, request
+import flask
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.cluster import KMeans
+
+
+stemmer=SnowballStemmer("english")
+stop = set(stopwords.words('english'))
+tokenizer = RegexpTokenizer(r'\w+')
+from random import shuffle
+
+def pdf_to_text(pdf):
+
+    pdfReader = PyPDF2.PdfFileReader(pdf)
+    numPage=pdfReader.numPages
+    pdftext=" "
+    for page in range(numPage):
+        pageObj =pdfReader.getPage(page)
+        text= pageObj.extractText()
+        pagetext="".join(text)
+        pdftext=" ".join([pagetext, pdftext])
+    
+
+    all_words= re.findall("[a-zA-Z]+", pdftext)
+    affterstemmer=[]
+    for word in all_words:
+        affterstemmer.append(stemmer.stem(word))
+    afterstop=[]
+    for word in affterstemmer:
+        if word not in stop:
+            afterstop.append(word)
+    if len(afterstop)>50:
+        afterstop.reverse()
+        
+        return " ".join(afterstop)
+
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+os.chdir(THIS_FOLDER)
+
+## Geting ready the training data
+data=pd.read_csv('readydata.csv')
+
+count_vect = CountVectorizer(lowercase = False, max_df = .6)
+tfidf_transformer = TfidfTransformer()
+X_train_counts = count_vect.fit_transform(data['0'])
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+# K-means model
+clf = KMeans(n_clusters=2,  max_iter=100 , random_state=0).fit(X_train_tfidf)
+def getPredictions(clf,count_vect,tfidf_transformer,X_test):
+
+    #do the training
+
+    X_test_counts = count_vect.transform(X_test)
+    X_test_tfidf = tfidf_transformer.transform(X_test_counts)
+
+    return clf.predict(X_test_tfidf)
+
+
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath("app.py"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, "testresumes/")
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+
+    for file in request.files.getlist("file"):
+        text=pdf_to_text(file)
+        X_test=[]
+        X_test.append(text)
+        result= getPredictions(clf,count_vect,tfidf_transformer,X_test)
+
+    if result[0]==0:
+        return render_template("result.html" ,sonuc="Your resume looks great! Here are some companies, You may want to consider: Apple, Hewlett-Packard, IBM, Amazon, Microsoft, Google, Intel, Cisco Systems, Oracle, Qualcomm, EMC, Xerox, Danaher, eBay, Uber, Plantair, Snapchat, Github, HackerRank, Twitter, Facebook, Texas Instruments, Quora, Intuit, Infosys, LinkedIn, Yahoo!, Kaspersky, Nvidia, AMD")
+    elif result[0]==1:
+        return render_template("result.html", sonuc="Your resume looks good but you need some improvement. Get experience from this companies: BMC Software, Pros Holding Inc., NetIQ, Quorum Business Solutions Inc, Alert Logic Inc., HCSS.")# waiting for tiers
+
+
+
+
+    return render_template("index.html")
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run()
